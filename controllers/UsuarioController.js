22f6d09f0449820ea4fec2dfd8c585e423a93455
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-09-14 22:15:45 -0300

    tutorial

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,8 +1,29 @@
+let path = require('path');
+
+let fs = require('fs');
+
 module.exports = {
     cadastro: function(req, res, next) {
         res.render('usuario/cadastro');
     },
     guardar: function(req, res, next) {
-        res.send('Salvando dados...');
+        // MODULO path
+        // responsável por montar caminho de arquivos.
+        // path.join("usuario", "cadastro");
+
+        // 
+        // JSON.parse 
+        // STRING PARA OBJETO JS
+        //
+        // JSON.stringfy
+        // CONVERTE OBJETO para String
+
+        // Para criptografar a senha utilizar a função bcrypt(VALOR_DA_SENHA)
+
+        // LE DADOS DE UM ARQUIVO.
+        fs.readFileSync('CAMINHO_DO_ARQUIVO');
+
+        // GRAVA DADOS EM UM ARQUIVO
+        fs.writeFileSync('CAMINHO_DO_ARQUIVO', "CONTEUDO EM STRING");
     }
 }
